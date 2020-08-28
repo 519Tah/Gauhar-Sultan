567f4b321e8e699d8fd98daf2d342c6b9a6c6333
Safi <safiullah.taher@ministrygroup.de>
2020-08-28 17:15:27 +0200

    main menu

--- header.php
+++ header.php
@@ -4,7 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="styles.css">
     <title>Gauhar-Sultan</title>
+    <link rel="stylesheet" type="text/css" href="styles/main.css">
 </head>
 <body>
+
+<header>
+    <div class="topnav">
+        <a class="active" href="#home">Home</a>
+        <a href="#blog">Blog</a>
+        <a href="#family-tree">Family Tree</a>
+        <a href="#profile">Profile</a>
+        <a href="#about">About</a>
+    </div> 
+</header>
--- index.php
+++ index.php
@@ -1,11 +1,56 @@
+<?php include ('header.php');?>
 
 
 
-<div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#blog">Blog</a>
-  <a href="#family-tree">Family Tree</a>
-  <a href="#profile">Profile</a>
-  <a href="#about">About</a>
-</div> 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include ('footer.php')?>
