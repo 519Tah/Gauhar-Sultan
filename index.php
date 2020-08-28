a8d78ac56b043d69d05a948d8d07508141a2a551
Safiullah Taher <Safiullah.Taher@ministry.local>
2020-08-28 16:04:16 +0200

    stucture changese

---
+++ index.php
@@ -0,0 +1,8 @@
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#blog">Blog</a>
+  <a href="#family-tree">Family Tree</a>
+  <a href="#profile">Profile</a>
+  <a href="#about">About</a>
+</div> 
+    
