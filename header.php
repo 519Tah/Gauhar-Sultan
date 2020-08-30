8978635da1e620416ce39d898a297d725202a1f9
safiullah taher <safiullah.taher@ministrygroup.de>
2020-08-30 22:03:30 +0200

    structure and log in front in

--- header.php
+++ header.php
@@ -12,9 +12,9 @@
 <header>
     <div class="topnav">
         <a class="active" href="#home">Home</a>
-        <a href="#blog">Blog</a>
-        <a href="#family-tree">Family Tree</a>
-        <a href="#profile">Profile</a>
-        <a href="#about">About</a>
+        <a href="blog.php">Blog</a>
+        <a href="family-tree.php">Family Tree</a>
+        <a href="profile.php">Profile</a>
+        <a href="about.php">About</a>
     </div> 
 </header>
